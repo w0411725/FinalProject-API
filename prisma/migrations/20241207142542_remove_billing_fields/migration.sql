338363b4765300dd336464c7476723b5753beb2f
Abdalla Elshafei <w0411725@nscc.ca>
2024-12-07 10:56:59 -0400

    Schema Update

---
+++ prisma/migrations/20241207142542_remove_billing_fields/migration.sql
@@ -0,0 +1,30 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `invoice_amt` on the `Purchase` table. All the data in the column will be lost.
+  - You are about to drop the column `invoice_tax` on the `Purchase` table. All the data in the column will be lost.
+  - You are about to drop the column `invoice_total` on the `Purchase` table. All the data in the column will be lost.
+
+*/
+-- RedefineTables
+PRAGMA defer_foreign_keys=ON;
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_Purchase" (
+    "purchase_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "customer_id" INTEGER NOT NULL,
+    "street" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "province" TEXT NOT NULL,
+    "country" TEXT NOT NULL,
+    "postal_code" TEXT NOT NULL,
+    "credit_card" TEXT NOT NULL,
+    "credit_expire" TEXT NOT NULL,
+    "credit_cvv" TEXT NOT NULL,
+    "order_date" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT "Purchase_customer_id_fkey" FOREIGN KEY ("customer_id") REFERENCES "Customer" ("customer_id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+INSERT INTO "new_Purchase" ("city", "country", "credit_card", "credit_cvv", "credit_expire", "customer_id", "order_date", "postal_code", "province", "purchase_id", "street") SELECT "city", "country", "credit_card", "credit_cvv", "credit_expire", "customer_id", "order_date", "postal_code", "province", "purchase_id", "street" FROM "Purchase";
+DROP TABLE "Purchase";
+ALTER TABLE "new_Purchase" RENAME TO "Purchase";
+PRAGMA foreign_keys=ON;
+PRAGMA defer_foreign_keys=OFF;

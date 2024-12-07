9760ef8d3f2cf8ab8cafcea152c00de031caeab8
Abdalla Elshafei <w0411725@nscc.ca>
2024-12-07 12:18:49 -0400

    Sprint 4 - Final Commit

--- app.js
+++ app.js
@@ -13,20 +13,21 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static('public'));
 
 app.use(cors({
-    credentials: true //allow cookies
-}))
+    origin: 'http://localhost:5173', 
+    credentials: true // Allows cookies/sessions to be shared
+  }));
 
-app.use(session({
-    secret:'gndoa#@ghiozaejhfr^&ea',
+  app.use(session({
+    secret: 'gndoa#@ghiozaejhfr^&ea',
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false, // Avoid creating sessions until needed
     cookie: {
-        httpOnly: true, 
-        secure: false, //true if using HTTPS
-        sameSite: 'lax', //should be none if client and server are on different origins
-        maxAge: 3600000 // 1hr
+      httpOnly: true, 
+      secure: false, // Set this to true if using HTTPS
+      sameSite: 'lax', // This must be lax to allow cross-origin session sharing
+      maxAge: 3600000 // 1 hour expiration
     }
-}))
+  }))
 
 // Test route
 // app.get('/', (req, res) => {
